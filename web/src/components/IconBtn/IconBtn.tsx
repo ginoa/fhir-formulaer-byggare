77c931ba351ddfd5f1293bd0946d84f40f558fd2
Reuben Ringdal <rubenr@aboveit.no>
2024-10-03 15:46:41 +0200

    added copy from json and format questionnaire response

--- web/src/components/IconBtn/IconBtn.tsx
+++ web/src/components/IconBtn/IconBtn.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import './IconBtn.css';
 
 type IconBtnProps = {
-    type?: 'back' | 'forward' | 'x' | 'info';
+    type?: 'back' | 'forward' | 'x' | 'info' | 'copy' | 'check';
     title?: string;
     onClick?: () => void;
     color?: 'white' | 'black';
     size?: 'large';
+    position?: 'left' | 'right';
+
 };
 
-const IconBtn = ({ type, title, onClick, color = 'white', size }: IconBtnProps): React.JSX.Element => {
+const IconBtn = ({ type, title, onClick, color = 'white', size, position }: IconBtnProps): React.JSX.Element => {
     const classNames: Array<string> = ['iconBtn'];
     if (color) {
         classNames.push(color);
@@ -17,9 +19,19 @@ const IconBtn = ({ type, title, onClick, color = 'white', size }: IconBtnProps):
     if (size) {
         classNames.push(size);
     }
-
+    if(position) {
+        classNames.push(position);
+    }
     let icon = '';
     switch (type) {
+        case 'check': {
+            icon = 'ion-checkmark';
+            break;
+        }
+        case 'copy': {
+            icon = 'ion-ios-copy';
+            break;
+        }
         case 'back': {
             icon = 'ion-ios-arrow-back';
             break;
