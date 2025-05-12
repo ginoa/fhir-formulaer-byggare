48012ef8cb0d46f8e38d97c273d8850d7e89a6e4
Arezoo Ghalichi <arezoo.ghalichi@nhn.no>
2025-05-12 15:48:49 +0200

    #351983 Skrives Url automatisk og validerer det

---
+++ web/src/helpers/validationClassHelper.ts
@@ -0,0 +1,40 @@
+import { ValidationError } from "src/utils/validationUtils";
+
+import { getValidationError } from "./validation/validationHelper";
+import { ErrorLevel } from "./validation/validationTypes";
+
+export const getTextValidationErrorClassName = (
+  error: ValidationError,
+): string => {
+  switch (error.errorLevel) {
+    case ErrorLevel.info:
+      return "msg-info";
+    case ErrorLevel.warning:
+      return "msg-warning";
+    default:
+      return "msg-error";
+  }
+};
+
+export const getValidaitonClassNameWithPropsName = (
+  propsname: string,
+  validationErrors: ValidationError[],
+): string => {
+  const error = getValidationError(propsname, validationErrors);
+  return getValidationClassName(error);
+};
+
+export const getValidationClassName = (
+  validationError: ValidationError | undefined,
+): string => {
+  switch (validationError?.errorLevel) {
+    case ErrorLevel.info:
+      return "validation-info";
+    case ErrorLevel.warning:
+      return "validation-warning";
+    case ErrorLevel.error:
+      return "validation-error";
+    default:
+      return "";
+  }
+};
